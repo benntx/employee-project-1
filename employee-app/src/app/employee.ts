b58dff643fb67f42b72cba5aa89588c617db168d
Alex Gorbachov <alexpitronot@gmail.com>
2021-11-24 18:29:15 +0200

    Exercise 2

--- employee-app/src/app/employee.ts
+++ employee-app/src/app/employee.ts
@@ -2,4 +2,7 @@ export interface Employee {
         id: number;
         fname: string;
         lname: string;
+        dateofbirth: string;
+        salary: string;
+        email: string;
       }    
--- employee-app/src/app/employees/employees.component.ts
+++ employee-app/src/app/employees/employees.component.ts
@@ -13,13 +13,37 @@ export class EmployeesComponent implements OnInit {
   employee: Employee = {
     id: 318033743,
     fname: 'Alex',
-    lname: 'Gorbachov'
+    lname: 'Gorbachov',
+    dateofbirth: "31/12/1977",
+    salary: '3,000 USD',
+    email: 'alexpitronot@gmail.com'
   };
 
   employee1: Employee = {
     id: 27261611,
     fname: 'Rotem',
-    lname: 'Malul'
+    lname: 'Malul',
+    dateofbirth: "30/12/1967",
+    salary: '4,000 USD',
+    email: 'rotem@gmail.com'
+  };
+
+  employee2: Employee = {
+    id: 27266701,
+    fname: 'Jack',
+    lname: 'Daniels',
+    dateofbirth: "30/12/1947",
+    salary: '5,000 USD',
+    email: 'jack@gmail.com'
+  };
+
+  employee3: Employee = {
+    id: 14331611,
+    fname: 'Aviva',
+    lname: 'Berg',
+    dateofbirth: "30/12/1987",
+    salary: '5,000 USD',
+    email: 'aviva@gmail.com'
   };
   
   constructor() { }

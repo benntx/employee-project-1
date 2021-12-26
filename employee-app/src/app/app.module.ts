ded639eb154ffdf7edd0ad44da57060edc627732
Alex Gorbachov <alexpitronot@gmail.com>
2021-12-26 18:48:02 +0200

    Update app.module.ts

--- employee-app/src/app/app.module.ts
+++ employee-app/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { FormsModule } from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EmployeesRoutingComponent } from './employees-routing/employees-routing.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-import { EmployeeSearchComponent } from './employee-search/employee-search.component';
-import { ExampleComponent } from './example/example.component';
+import { NgModule } from '@angular/core'; //default
+import { BrowserModule } from '@angular/platform-browser'; //default
+import { AppComponent } from './app.component'; //*
+import { EmployeesComponent } from './employees/employees.component'; //*
+import { FormsModule } from '@angular/forms'; //forms
+import { FooterComponent } from './footer/footer.component'; //*
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component'; //*
+import { MessagesComponent } from './messages/messages.component'; //*
+import { AppRoutingModule } from './app-routing.module'; //*
+import { DashboardComponent } from './dashboard/dashboard.component'; //*
+import { EmployeesRoutingComponent } from './employees-routing/employees-routing.component'; //*
+import { HttpClientModule } from '@angular/common/http'; //routing
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; //*
+import { InMemoryDataService } from './in-memory-data.service'; //*
+import { EmployeeSearchComponent } from './employee-search/employee-search.component'; //*
+import { ExampleComponent } from './example/example.component'; //*
 
 @NgModule({
   declarations: [
